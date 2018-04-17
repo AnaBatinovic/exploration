8f07034a4fd7675ad6c2de2fe4376b5159d88d7b
ivonaj <ivonaj1@gmail.com>
2018-04-17 12:17:24 +0200

    Migrate commits from ivonaj/rrt_exploration
        5c7edad Tranform filtered points, add robot namespace
        c3a0ac3 limit search for random points within occgridmap
        b6f71cf change to cartographer::transform::Rigid3
        364ffcb working global rrt detector
        dc9e16a add support for different trajectory ids
        7adf990 update RRT tree, add markers
        148872e publishing transformed frontiers to detected_points
        12cda87 begin writing frontier to submap transformation fcn
        be3f231 add submap callback
        3de9e02 change to c++11 compiler
        78602d6 add changes
        c098871 try new filtering
        2139978 delete bad frontiers
        4b325d3 Add obstacle check for accumulated centroids
        f5eb0d6 Change filter parameters
        dab9a93 correct service name, add small adjustments of topic names
        18b18d9 Change obstacleFree detection ranges, add neighborhood cell check
        915ffd2 customize for simulation

--- rrt_exploration/include/functions.h
+++ rrt_exploration/include/functions.h
@@ -8,7 +8,15 @@
 #include "nav_msgs/OccupancyGrid.h"
 #include "geometry_msgs/Point.h"
 #include "visualization_msgs/Marker.h"
-
+#include "cartographer_ros_msgs/SubmapList.h"
+#include "geometry_msgs/TransformStamped.h"
+#include "geometry_msgs/PointStamped.h"
+#include "geometry_msgs/Vector3.h"
+#include <tf/tf.h>
+#include"rrt_exploration/FrontierTF.h"
+#include <tuple>
+#include "cartographer/transform/rigid_transform.h"
+#include "cartographer_ros/msg_conversion.h"
 // rdm class, for gentaring random flot numbers
 class rdm{
 int i;
@@ -17,22 +25,30 @@ rdm();
 float randomize();
 };
 
-
 //Norm function prototype
 float Norm( std::vector<float> , std::vector<float> );
 
 //sign function prototype
 float sign(float );
-
+//init map for exploration
+void initMap(nav_msgs::OccupancyGrid&,float&, float&,float&, float&,visualization_msgs::Marker&);
 //Nearest function prototype
-std::vector<float> Nearest(  std::vector< std::vector<float>  > , std::vector<float> );
+std::tuple<std::vector<float>, int> Nearest(  std::vector< std::vector<float>  > , std::vector<float> );
+std::tuple<std::vector<float>,int> NearestSameId(int&,std::vector<rrt_exploration::FrontierTF>&,rrt_exploration::FrontierTF,cartographer_ros_msgs::SubmapList);
 
+std::vector<float> TF2XY(cartographer_ros_msgs::SubmapList,rrt_exploration::FrontierTF);
 //Steer function prototype
 std::vector<float> Steer(  std::vector<float>, std::vector<float>, float );
 
 //gridValue function prototype
 int gridValue(nav_msgs::OccupancyGrid &,std::vector<float>);
 
+char checkNeighbours(nav_msgs::OccupancyGrid &,std::vector<float>);
+
 //ObstacleFree function prototype
 char ObstacleFree(std::vector<float> , std::vector<float> & , nav_msgs::OccupancyGrid);
+rrt_exploration::FrontierTF Point2Tf(cartographer_ros_msgs::SubmapList,std::vector<float>);
+
+std::vector< std::vector<float>> refreshTree(cartographer_ros_msgs::SubmapList,std::vector<rrt_exploration::FrontierTF>);
+void DrawRRT(visualization_msgs::Marker&,std::vector<std::tuple<int,int>>,std::vector< std::vector<float>>);
 #endif
--- rrt_exploration/src/functions.cpp
+++ rrt_exploration/src/functions.cpp
@@ -1,5 +1,6 @@
 #include "functions.h"
 
+using Rigid3d=cartographer::transform::Rigid3d;
 
 // rdm class, for gentaring random flot numbers
 rdm::rdm() {i=time(0);}
@@ -21,27 +22,128 @@ if (n<0.0){return -1.0;}
 else{return 1.0;}
 }
 
+void initMap(nav_msgs::OccupancyGrid& mapData,float& init_map_x, float& init_map_y,float& Xstartx, float& Xstarty,visualization_msgs::Marker& points){
 
+    geometry_msgs::Pose OccupancyOrigin=mapData.info.origin;
+    const Rigid3d rigidTransf=cartographer_ros::ToRigid3d(OccupancyOrigin);
+    Rigid3d TFpoint=rigidTransf*Rigid3d::Translation(
+            Rigid3d::Vector(0.0,mapData.info.height*mapData.info.resolution,0.0));
+    points.points.push_back(cartographer_ros::ToGeometryMsgPose(TFpoint).position);
+    TFpoint=rigidTransf*Rigid3d::Translation(
+            Rigid3d::Vector(0.0,0.0,0.0));
+    points.points.push_back(cartographer_ros::ToGeometryMsgPose(TFpoint).position);
+    TFpoint=rigidTransf*Rigid3d::Translation(
+            Rigid3d::Vector(mapData.info.width*mapData.info.resolution,0.0,0.0));
+    points.points.push_back(cartographer_ros::ToGeometryMsgPose(TFpoint).position);
+    TFpoint=rigidTransf*Rigid3d::Translation(
+            Rigid3d::Vector(mapData.info.width*mapData.info.resolution,mapData.info.height*mapData.info.resolution,0.0));
+    points.points.push_back(cartographer_ros::ToGeometryMsgPose(TFpoint).position);
+
+    std::vector<float> temp1;
+    temp1.push_back(points.points[0].x);
+    temp1.push_back(points.points[0].y);
+
+    std::vector<float> temp2;
+
+    temp2.push_back(points.points[2].x);
+    temp2.push_back(points.points[0].y);
+
+
+    init_map_x=Norm(temp1,temp2);
+    temp1.clear();		temp2.clear();
+
+    temp1.push_back(points.points[0].x);
+    temp1.push_back(points.points[0].y);
+
+    temp2.push_back(points.points[0].x);
+    temp2.push_back(points.points[2].y);
+
+    init_map_y=Norm(temp1,temp2);
+    temp1.clear();		temp2.clear();
+
+    Xstartx=(points.points[0].x+points.points[2].x)*.5;
+    Xstarty=(points.points[0].y+points.points[2].y)*.5;
+
+
+}
 //Nearest function
-std::vector<float> Nearest(  std::vector< std::vector<float>  > V, std::vector<float>  x){
+std::tuple<std::vector<float>,int> Nearest(  std::vector< std::vector<float>  > V, std::vector<float>  x){
 
-float min=Norm(V[0],x);
-int min_index;
-float temp;
+    float min=Norm(V[0],x);
+    int min_index;
+    float temp;
 
-for (int j=0;j<V.size();j++)
-{
-temp=Norm(V[j],x);
-if (temp<=min){
-min=temp;
-min_index=j;}
+    for (int j=0;j<V.size();j++)
+    {
+        temp=Norm(V[j],x);
+        if (temp<=min){
+            min=temp;
+            min_index=j;}
+
+    }
+
+    return std::make_tuple(V[min_index],min_index) ;
+}
+std::tuple<std::vector<float>,int> NearestSameId(int& init_id,std::vector<rrt_exploration::FrontierTF>& pointsTF,rrt_exploration::FrontierTF point,cartographer_ros_msgs::SubmapList SubmapList_){
+    int i=0;
+    float min = std::numeric_limits<float>::max();;
+    int min_index;
+    float temp;
+    std::vector<float> best;
+    std::vector<float> XYpoint1,XYpoint2;
+
+    XYpoint1=TF2XY(SubmapList_,point);
+    for (auto& TFpoint : pointsTF) {
+        if(TFpoint.trajectory_id==point.trajectory_id){
+            XYpoint2=TF2XY(SubmapList_,TFpoint);
+            temp=Norm(XYpoint1,XYpoint2);
+            if (temp<=min){
+                min=temp;
+                std::cout<<min<<std::endl;
+                min_index=i;
+                best=XYpoint2;
+            }
+            init_id=1;
+        }
+        i++;
+    }
+    if (init_id==0){
+        rrt_exploration::FrontierTF added;
+        for (auto& submap_msg : SubmapList_.submap) {
+            if (submap_msg.trajectory_id==point.trajectory_id){
+                added.trajectory_id=submap_msg.trajectory_id;
+                added.submapIndex=submap_msg.submap_index;
+                geometry_msgs::Vector3 vec;
+                vec.x=0;
+                vec.y=0;
+                vec.z=0;
+                added.transform=vec;
+                break;
+            }
+        }
+
+        pointsTF.push_back(added);
+        std::cout<<"dodaj"<<std::endl;
+        //return dummy values
+        min_index=i;
+        best=XYpoint2;
+
+    }
+    return std::make_tuple(best,min_index) ;
 
 }
 
-return V[min_index];
+std::vector<float> TF2XY(cartographer_ros_msgs::SubmapList SubmapList_,rrt_exploration::FrontierTF pointTF){
+    std::vector<float> PointXY;
+    for (auto& submap_msg : SubmapList_.submap) {
+        if((submap_msg.trajectory_id==pointTF.trajectory_id) && (submap_msg.submap_index==pointTF.submapIndex)){
+            PointXY.push_back(pointTF.transform.x+submap_msg.pose.position.x);
+            PointXY.push_back(pointTF.transform.y+submap_msg.pose.position.y);
+            break;
+        }
+    }
+    return PointXY;
 }
-
-
 
 //Steer function
 std::vector<float> Steer(  std::vector<float> x_nearest , std::vector<float> x_rand, float eta){
@@ -70,9 +172,6 @@ return x_new;
 }
 
 
-
-
-
 //gridValue function
 int gridValue(nav_msgs::OccupancyGrid &mapData,std::vector<float> Xp){
 
@@ -87,12 +186,38 @@ std::vector<signed char> Data=mapData.data;
 //map data:  100 occupied      -1 unknown       0 free
 float indx=(  floor((Xp[1]-Xstarty)/resolution)*width)+( floor((Xp[0]-Xstartx)/resolution) );
 int out;
+std::cout<<"Dobio "<<Xp[0]<<" "<<Xp[1]<<std::endl;
+
+std::cout<<"Ne volim index "<<indx<<std::endl;
+
 out=Data[int(indx)];
+std::cout<<"GridValue output "<<out<<std::endl;
 return out;
 }
 
+inline float getCellIndex(float cell_x_new,float cell_y_new, float width) {return ( cell_y_new*width)+cell_x_new;}
+//search for outliers by checking neighbours of free space cells
+void checkNeighbours(nav_msgs::OccupancyGrid &mapData,std::vector<float> Xp, char& obs){
 
 
+    //std::vector<float> neigbours;
+    float resolution=mapData.info.resolution;//m/cell
+    float Xstartx=mapData.info.origin.position.x;
+    float Xstarty=mapData.info.origin.position.y;
+
+    float width=mapData.info.width;
+    float height=mapData.info.height;
+    std::vector<signed char> Data=mapData.data;
+
+    float cell_y=floor((Xp[1]-Xstarty)/resolution);
+    float cell_x=floor((Xp[0]-Xstartx)/resolution);
+
+    for (int i=-1;i<=1;i++){
+        for(int j=-1;j<=1;j++){
+            if (Data[int(getCellIndex(cell_x+i,cell_y+j,width))]>30) obs = 1;
+        }
+    }
+}
 
 // ObstacleFree function-------------------------------------
 
@@ -100,32 +225,113 @@ char ObstacleFree(std::vector<float> xnear, std::vector<float> &xnew, nav_msgs::
 float rez=float(mapsub.info.resolution)*.2;
 float stepz=int(ceil(Norm(xnew,xnear))/rez); 
 std::vector<float> xi=xnear;
-char  obs=0; char unk=0;
- 
+char  obs=0; char unk=0; char free=0;
+std::cout<<"enter ObstacelFree"<<std::endl;
+
 geometry_msgs::Point p;
 for (int c=0;c<stepz;c++){
   xi=Steer(xi,xnew,rez);
-  		
 
-   if (gridValue(mapsub,xi) ==100){     obs=1; }
-   
-   if (gridValue(mapsub,xi) ==-1){      unk=1;	break;}
+    std::cout<<"check grid value"<<std::endl;
+
+    if (gridValue(mapsub,xi) >=30) {     obs=1; }
+
+    if (gridValue(mapsub,xi) ==-1){      unk=1;	break;}
+
+    if (gridValue(mapsub,xi)<30) {free=1;
+
+    }
+    std::cout<<"pass grid val check"<<std::endl;
+
   }
+    std::cout<<"calc out"<<std::endl;
+
+checkNeighbours(mapsub,xi,obs);
 char out=0;
  xnew=xi;
  if (unk==1){  out=-1;}
  	
  if (obs==1){  out=0;}
  		
- if (obs!=1 && unk!=1){   out=1;}
+ if (unk!=1 && free==1 && obs!=1 ){   out=1;}
 
- 
- 
- 
  return out;
- 
-
  }
+
+rrt_exploration::FrontierTF Point2Tf(cartographer_ros_msgs::SubmapList SubmapList_,std::vector<float> point) {
+    bool init=true;
+    float min_dist;
+    float dist;
+    cartographer_ros_msgs::SubmapEntry best_submap;
+    rrt_exploration::FrontierTF newTFPoint;
+    for (auto& submap_msg : SubmapList_.submap) {
+        std::vector<float> submap_point;
+        submap_point.push_back(submap_msg.pose.position.x);
+        submap_point.push_back(submap_msg.pose.position.y);
+        dist=Norm(submap_point,point);
+        if(init==true){
+            min_dist=dist;
+            best_submap=submap_msg;
+            init=false;
+        }
+        else {
+            if(min_dist>dist){
+            min_dist=dist;
+            best_submap=submap_msg;
+            }
+        }
+    }
+    std::cout<<"Robotu "<<best_submap.trajectory_id<<" najbliza submapa "<<best_submap.submap_index<<std::endl;
+    const Rigid3d rigidTransf=cartographer_ros::ToRigid3d(best_submap.pose);
+    const Rigid3d TFpoint=rigidTransf.inverse()*Rigid3d::Translation(
+            Rigid3d::Vector(point[0],point[1],0.0));
+    newTFPoint.trajectory_id=best_submap.trajectory_id;
+    newTFPoint.submapIndex=best_submap.submap_index;
+    newTFPoint.transform.x=cartographer_ros::ToGeometryMsgPose(TFpoint).position.x;
+    newTFPoint.transform.y=cartographer_ros::ToGeometryMsgPose(TFpoint).position.y;
+    newTFPoint.transform.z=0.0;
+
+
+    return newTFPoint;
+
+}
+std::vector< std::vector<float>> refreshTree(cartographer_ros_msgs::SubmapList SubmapList_,std::vector<rrt_exploration::FrontierTF> pointsTF){
+    std::vector< std::vector<float>> RRT;
+    for(auto point:pointsTF){
+        for (auto& submap_msg : SubmapList_.submap) {
+            if((submap_msg.trajectory_id==point.trajectory_id) && (submap_msg.submap_index==point.submapIndex)){
+                const Rigid3d rigidTransf=cartographer_ros::ToRigid3d(submap_msg.pose);
+                const Rigid3d TFpoint=rigidTransf*Rigid3d::Translation(
+                        Rigid3d::Vector(point.transform.x,point.transform.y,point.transform.z));
+
+                RRT.push_back({TFpoint.translation().x(),
+                               TFpoint.translation().y()});
+                break;
+            }
+        }
+    }
+    return RRT;
+}
+void DrawRRT(visualization_msgs::Marker& line,std::vector<std::tuple<int,int>>indexes,std::vector< std::vector<float>> points){
+    line.points.clear();
+    int one,two;
+    geometry_msgs::Point p;
+    std::vector<float> point;
+    for (auto ind:indexes){
+        std::tie(one,two)=ind;
+        point=points.at(one);
+        p.x=point[0];
+        p.y=point[1];
+        p.z=0.0;
+        line.points.push_back(p);
+        point=points.at(two);
+        p.x=point[0];
+        p.y=point[1];
+        p.z=0.0;
+        line.points.push_back(p);
+    }
+}
+
  
 
 
