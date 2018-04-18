81ed0b74079bedb88763618e80080f6ed9810b2e
ivonaj <ivonaj1@gmail.com>
2018-04-18 16:20:26 +0200

    change server address

--- multi_robot_exploration/config/cloud.lua
+++ multi_robot_exploration/config/cloud.lua
@@ -15,6 +15,6 @@
 include "map_builder_server.lua"
 
 MAP_BUILDER_SERVER.map_builder.use_trajectory_builder_2d = true
-MAP_BUILDER_SERVER.server_address ="localhost:55555"
+MAP_BUILDER_SERVER.server_address ="0.0.0.0:55555"
 
 return MAP_BUILDER_SERVER
